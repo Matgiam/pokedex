be9d5a262a902bd4c60cef669ec7a6d6697a5865
Matteo <Matteo.giambarresi0504@gmail.com>
2025-08-08 14:31:36 +0200

    Setup the basic routing

---
+++ src/pages/PokemonDetail.jsx
@@ -0,0 +1,9 @@
+function PokemonDetail(){
+    return (
+        <div>
+            <h1>Detail</h1>
+        </div>
+    )
+}
+
+export default PokemonDetail;
---
+++ src/routing/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import Home from "../pages/Home"
+
+function App() { 
+
+  return (
+    <>
+     <BrowserRouter>
+     <Routes>
+     <Route path='/' element = {<Home/>}/>
+     <Route index element = {<Home/>}/>
+     </Routes>
+     </BrowserRouter>
+    </>
+  );
+}
+
+export default App
