b8a5856e98631504ac5b42962f5379ff45176017
Matteo <Matteo.giambarresi0504@gmail.com>
2025-08-08 16:14:05 +0200

    Added the generation functionality where are the pokemons are sorted with their generation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,14 +5,22 @@ import SearchFilter from "../components/SearchFilter";
 import PokemonCard from '../components/PokemonCard';
 function Home (){
     const [searchTerm, setSearchTerm] = useState("")
+    const generations = [1,2,3,4,5];
     const filteredPokemon = DATA.filter((pokemon)=>
     pokemon.name.toLowerCase().includes(searchTerm.toLowerCase()))
     return (
  <div>
   <h1>Pokedex</h1>
   <SearchFilter onSearch={setSearchTerm}/>
+  {generations.map((gen) =>{
+    const genPokemon = filteredPokemon.filter((pokemon)=> pokemon.generation === gen)
+    if (genPokemon.length === 0) return null;
+
+  return(
+   <div key={gen}>
+  <h2>Generation {gen} </h2>
   <div className="pokemon-list">
-    {filteredPokemon.map((pokemon)=>(
+    {genPokemon.map((pokemon)=>(
             
 <div key={pokemon.id}className="pokemonCard">
 <PokemonCard id={pokemon.id} name={pokemon.name} image={pokemon.image}/>
@@ -20,8 +28,12 @@ function Home (){
 
   ))}
   </div>
+   </div>
+  );
+    })}
 </div>
     )
 }
 
 export default Home;
+
--- src/pages/PokemonDetail.jsx
+++ src/pages/PokemonDetail.jsx
@@ -3,11 +3,16 @@ import { useParams, Link, useFetcher } from 'react-router';
 import DATA from "../data/pokemon.json"
 function PokemonDetail(){
     const {id} = useParams();
-   const pokemonId = DATA.find((p) => p.id == id)
+   const pokemon = DATA.find((p) => p.id == id)
     return (
         <div>
-            <h1>{pokemonId.name}</h1>
-          
+            <h1> Generation: {pokemon.generation}</h1>
+            <h1>{pokemon.name}</h1>
+            <img src={pokemon.image} alt="" />
+            <h2>{pokemon.type}</h2>
+     <Link to={`/`}>
+<button>Go back</button>
+</Link>
         </div>
     )
 }
